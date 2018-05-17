e315d01ff4071e6ab65e02bac6953ad7f5a68f75
tuochao <ctuo@thoughtworks.com>
2018-05-17 17:05:03 +0800

    Change cleanup stage.

--- ci/cleanup.sh
+++ ci/cleanup.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-docker stop web-hp web-srp-pdp
-docker rm web-hp web-srp-pdp
-docker rmi web-test
-docker rmi ${REPOSITORY_URL}/${HP_REPO_NAME}:${VERSION_HP}
-docker rmi ${REPOSITORY_URL}/${SRP_PDP_REPO_NAME}:${VERSION_SRPPDP}
+docker stop web-automation
+docker rm web-automation
+docker rmi web-automation
